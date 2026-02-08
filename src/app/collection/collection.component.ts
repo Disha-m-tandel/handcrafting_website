a10790b28d50a638b69b4f2e749417e0959de831
VSAAS_First-Angular-Project <disha@onnetsystems.net>
2026-02-08 19:24:49 +0530

    design changes

--- src/app/collection/collection.component.ts
+++ src/app/collection/collection.component.ts
@@ -17,11 +17,12 @@ export class CollectionComponent {
 
   products : Product[] =[
     { id : 1, name: 'Floral Hoop', image: 'assets/images/1.webp' },
-    { id : 2, name: 'Floral Hoop', image: 'assets/images/2.webp' },
-    { id : 3, name: 'Floral Hoop', image: 'assets/images/3.webp' },
-    { id : 5, name: 'Floral Hoop', image: 'assets/images/5.webp' },
+    { id : 4, name: 'Floral Hoop', image: 'assets/images/4.jpg' },
     { id : 6, name: 'Floral Hoop', image: 'assets/images/6.webp' },
-    { id : 7, name: 'Floral Hoop', image: 'assets/images/7.webp' },
+    { id : 2, name: 'Floral Hoop', image: 'assets/images/2.webp' },
+    { id : 5, name: 'Floral Hoop', image: 'assets/images/5.jpg' },
+    { id : 3, name: 'Floral Hoop', image: 'assets/images/3.webp' },
+    { id : 7, name: 'Floral Hoop', image: 'assets/images/7.jpg' },
   ];
   
 
@@ -40,7 +41,7 @@ removeFromCart(product: Product) {
     `Hi, I like this design:\n${product.name}\n${window.location.origin}/${product.image}`;
 
   window.open(
-    `https://wa.me/91XXXXXXXXXX?text=${encodeURIComponent(message)}`,
+    `https://wa.me/919113872844?text=${encodeURIComponent(message)}`,
     '_blank'
   );
 }
